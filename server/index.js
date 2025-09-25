1c580cde92a633de0b3ce184db229015cf174e6f
MokshaOdlamaani2 <moksha2422@gmail.com>
2025-09-25 18:54:31 +0530

    connection code fixed

--- server/index.js
+++ server/index.js
@@ -31,11 +31,9 @@ app.get('/', (req, res) => {
 
 // Connect DB and start server
 const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+
+ mongoose.connect(process.env.MONGO_URI)
+
   .then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
