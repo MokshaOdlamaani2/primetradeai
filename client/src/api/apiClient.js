8eae6716985d086dbdd6042b2c546bb4de81af01
MokshaOdlamaani2 <moksha2422@gmail.com>
2025-09-25 19:30:24 +0530

    connection code fixed

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,7 +1,10 @@
 // src/api/apiClient.js
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+
+const API_BASE = process.env.REACT_APP_API_URL || 'https://primetradeaiserver.onrender.com/api';
+
 
 const apiClient = axios.create({
   baseURL: API_BASE,
